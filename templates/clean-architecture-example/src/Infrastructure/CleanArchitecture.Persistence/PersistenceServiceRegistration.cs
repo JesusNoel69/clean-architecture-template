125742c030cc99b716192fcb1e2a0e0f1759b8a0
Jesus <jnoel883@gmail.com>
2026-05-20 18:45:17 -0600

    Remove usings wrong writed

--- templates/clean-architecture-example/src/Infrastructure/CleanArchitecture.Persistence/PersistenceServiceRegistration.cs
+++ templates/clean-architecture-example/src/Infrastructure/CleanArchitecture.Persistence/PersistenceServiceRegistration.cs
@@ -1,7 +1,7 @@
-﻿using Core.CleanArchitecture.Application.Interface.Persistence;
+﻿/*using Core.CleanArchitecture.Application.Interface.Persistence;
 using Core.CleanArchitecture.Domain;
 using CleanArchitecture.Persistence.DatabaseContext;
-using CleanArchitecture.Persistence.Repositories;
+using CleanArchitecture.Persistence.Repositories;*/
 namespace CleanArchitecture.Persistence;
 
 public static class PersistenceServiceRegistration 
