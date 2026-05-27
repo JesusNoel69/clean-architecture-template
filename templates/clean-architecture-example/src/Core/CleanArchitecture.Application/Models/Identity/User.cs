95dd421677408c151432e6d33843e0f9bf868723
Jesus <jnoel883@gmail.com>
2026-05-26 20:38:58 -0600

    Add roles and UserName into User model

--- templates/clean-architecture-example/src/Core/CleanArchitecture.Application/Models/Identity/User.cs
+++ templates/clean-architecture-example/src/Core/CleanArchitecture.Application/Models/Identity/User.cs
@@ -4,7 +4,9 @@ namespace CleanArchitecture.Application.Models.Identity
     {
         public string Id { get; set; }
         public string Email { get; set; }
+        public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public IList<string> Roles { get; set; } = [];
     }
 }
